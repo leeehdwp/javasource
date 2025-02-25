e65cb20b063862284328793712e6d08dadefc8bb
leedongjae <dongje232@gmail.com>
2025-02-25 17:24:03 +0900

    연산자 프로젝트

---
+++ ch2/src/op/IncDecOperEX1.java
@@ -0,0 +1,30 @@
+package op;
+// 증감연산자 : +1,-1 간단하게 정리
+// ++,-- : 변수 앞(전위), 변수 뒤(후위)
+
+
+public class IncDecOperEX1 {
+public static void main(String[] args) { 
+
+     int num = 10;
+
+     num = num + 1;
+     System.out.println("num = "+num);    
+     
+     //num = num - 1;
+     --num;
+     System.out.println("num = "+num);
+
+     
+     int num2 = 20;
+     System.out.println("num ="+num2);
+
+    
+     
+    }   
+}
+
+
+    
+
+    
